cc1b19f8fa2fe30f39bd87779721429392a16fe6
Gornichev <gornichev1986@gmail.com.noreply.github.com>
2022-01-21 22:39:39 +0200

    btn header script

--- int/js/main.js
+++ int/js/main.js
@@ -17,3 +17,24 @@ hideMenu.onclick = function() {
     menu.classList.remove("show");
 }
 
+
+
+
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+
+        const dropdowns = document.getElementsByClassName("dropdown-content");
+        let i;
+        for (i = 0; i < dropdowns.length; i++) {
+            const openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
